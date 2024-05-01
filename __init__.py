0b6c7585e368bf8b91f0b5f54194d2fc940afaaa
kexin jiang <mac@kexins-MacBook-Air.local>
2024-05-01 18:41:35 +0800

    flask

---
+++ __init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+def create_app(config_name=None):
+    app = Flask('bbs')
+
+    @app.route('/')
+    def index():
+        return render_template('frontend/index.html')
+    
+    register_error_handlers(app)
+    return app
+
+def register_error_handlers(app: Flask):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('error/400.html'), 400
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('error/403.html'), 403
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template('error/404.html'), 404
+
+    @app.errorhandler(500)
+    def server_error(e):
+        return render_template('error/500.html'), 500
