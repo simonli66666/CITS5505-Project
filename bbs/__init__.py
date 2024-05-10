d1dc77e73974435aa49dd4fa034e39b8b8147de4
Chiaraliz <15962100649@163.com>
2024-05-10 16:08:20 +0800

    modify share recipe box, add js to message box

---
+++ bbs/__init__.py
@@ -0,0 +1,159 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from bbs.extensions import db
+from bbs.models import *
+from bbs.setting import *
+import click
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
+
+
+
+
+# app = Flask('bbs')
+app = Flask(__name__, static_folder='static')
+
+
+app.config['SECRET_KEY'] = 'admin'
+
+@app.route('/')
+def index():
+    return render_template('frontend/index.html')
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'index'  # 未登录时重定向到的视图函数
+login_manager.login_message_category = 'info'
+
+# Flask-Login 用户加载器
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# 注册路由
+@app.route('/register1', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        nickname = request.form['nickname']
+        password = request.form['password']
+        # 检查用户名是否已存在
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists!', 'error')
+            return render_template('frontend/index.html', error=True, username=username)
+        if User.query.filter_by(nickname=nickname).first():
+            flash('Username already exists!', 'error')
+            return render_template('frontend/index.html', error=True, nickname=nickname)
+        new_user = User(username=username,nickname=nickname, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('User created successfully!','success')
+        return redirect(url_for('login'))
+    return render_template('frontend/index.html', error=False) 
+
+
+
+# 登录路由
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+        
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('recipes')) 
+        else:
+            flash('Invalid username or password!', 'error')
+    return render_template('frontend/index.html')
+
+# 发布菜单路由
+@app.route('/share', methods=['GET', 'POST'])
+def share():
+    if request.method == 'POST':
+        title = request.form['title']
+        image = request.form['image']
+        ingredient1 = request.form['ingredient1']
+        ingredient2 = request.form['ingredient2']
+        ingredient3 = request.form['ingredient3']
+        ingredient4 = request.form['ingredient4']
+        ingredient5 = request.form['ingredient5']
+        ingredient6 = request.form['ingredient6']
+        servings = request.form['servings']
+        prep_time = request.form['prep_time']
+        cooking_time = request.form['cooking_time']
+        calories = request.form['calories']
+        content = request.form['content']
+       
+        # 检查用户名是否已存在
+        
+        new_post = Post(title=title,image=image, ingredient1=ingredient1,ingredient2=ingredient2, ingredient3=ingredient3, ingredient4=ingredient4, ingredient5=ingredient5, ingredient6=ingredient6,servings= servings, prep_time=prep_time, cooking_time=cooking_time, calories=calories, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Recipt posted successfully!','success')
+        return redirect(url_for('recipes'))
+    return render_template('frontend/login.html', error=False)
+
+
+
+@app.route('/recipes')
+@login_required
+def recipes():
+    return render_template('frontend/login.html')
+
+@app.route('/user')
+@login_required
+def user():
+    return render_template('frontend/user.html')
+
+# 帖子详情页面
+@app.route('/posts_details')
+def posts_details():
+    return render_template('frontend/postsDetails.html')
+
+
+
+@app.route('/badges')
+@login_required
+def badges():
+    return render_template('frontend/badges.html')
+
+def register_error_handlers(app: Flask):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('error/400.html'), 400
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('error/403.html'), 403
+
+    @app.errorhandler(404)
+    def not_found(e):
+        return render_template('error/404.html'), 404
+
+    @app.errorhandler(500)
+    def server_error(e):
+        return render_template('error/500.html'), 500
+
+
+def register_extensions(app: Flask):
+    db.init_app(app)
+
+
+def register_cmd(app: Flask):
+    @app.cli.command()
+    def admin():
+        click.confirm('这个操作会清空整个数据库,要继续吗?', abort=True)
+        db.drop_all()
+        click.echo('清空数据库完成!')
+        db.create_all()
+        click.echo('数据库初始化完成!')
+
+
+
+register_error_handlers(app)
+app.config.from_object(DevelopmentConfig)
+register_extensions(app)
+register_cmd(app)
---
+++ bbs/models.py
@@ -0,0 +1,45 @@
+import datetime
+
+from bbs.extensions import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    __tablename__ = 't_user'
+
+    id = db.Column(db.INTEGER, primary_key=True, nullable=False, index=True, autoincrement=True)
+    username = db.Column(db.String(40), nullable=False, index=True, unique=True, comment='user name')
+    nickname = db.Column(db.String(40), nullable=False, unique=True, comment='user nick name')
+    password = db.Column(db.String(256), comment='user password')
+    create_time = db.Column(db.DATETIME, default=datetime.datetime.now)
+
+    posts = db.relationship('Post', back_populates='user')
+
+class Post(db.Model):
+     __tablename__ = 't_post'
+
+     id = db.Column(db.INTEGER, primary_key=True, autoincrement=True, index=True)
+     title = db.Column(db.String(100), index=True, nullable=False)
+     image = db.Column(db.String(255), nullable=True)
+     ingredient1 = db.Column(db.TEXT, nullable=False)
+     ingredient2 = db.Column(db.TEXT, nullable=True)
+     ingredient3 = db.Column(db.TEXT, nullable=True)
+     ingredient4 = db.Column(db.TEXT, nullable=True)
+     ingredient5 = db.Column(db.TEXT, nullable=True)
+     ingredient6 = db.Column(db.TEXT, nullable=True)
+     servings = db.Column(db.INTEGER, default=0)
+     prep_time = db.Column(db.FLOAT, default=0, comment='Preparation time in minutes')
+     cooking_time = db.Column(db.FLOAT, default=0, comment='Cooking time in minutes')
+     calories = db.Column(db.INTEGER, default=0)
+     content = db.Column(db.TEXT, nullable=False)
+     create_time = db.Column(db.DateTime, default=datetime.datetime.now)
+     update_time = db.Column(db.DateTime, default=datetime.datetime.now)
+     read_times = db.Column(db.INTEGER, default=0)
+     # 用户对帖子的操作
+     likes = db.Column(db.INTEGER, default=0, comment='like post persons')
+     comment_num = db.Column(db.INTEGER, default=0, comment='comment numbers')
+     
+     # 外键id
+     author_id = db.Column(db.INTEGER, db.ForeignKey('t_user.id'))
+     
+     # 用户关系
+     user = db.relationship('User', back_populates='posts')
